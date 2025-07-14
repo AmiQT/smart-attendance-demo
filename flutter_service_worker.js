9f20a5bda684bb9dd3ef58671b4569f84add241a
AmiQT <81741116+AmiQT@users.noreply.github.com>
2025-07-15 00:55:07 +0800

    Remove Mock Locater with real provider

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,14 +22,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "bfdf9c614e42fb5333a6abced5416408",
+"flutter_bootstrap.js": "aae8004a7bb2137a6cda5f98cc986d5d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "c1a142cf09edc55c96e5613a880bd283",
 "/": "c1a142cf09edc55c96e5613a880bd283",
-"main.dart.js": "815498aced146b6fbd11556877ef27be",
+"main.dart.js": "237b8e1af62a90fe6ccbcc519683b6e9",
 "manifest.json": "0bfbf4580eb08b946e2e9a5de334a9c2",
 "version.json": "fd6272a695cdf15ab87c6634f567552b"};
 // The application shell files that are downloaded before a service worker can
